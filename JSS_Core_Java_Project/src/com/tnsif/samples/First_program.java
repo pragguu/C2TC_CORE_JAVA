946c7c33f9801bdf141091887e16733cf5f3f5b1
pragguu <aithalprajnar@gmail.com>
2025-07-16 19:01:22 +0530

    First Commit

---
+++ JSS_Core_Java_Project/src/com/tnsif/samples/First_program.java
@@ -0,0 +1,11 @@
+package com.tnsif.samples;
+
+public class First_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World!!");
+System.out.println("Welcome to java Programming");
+	}
+
+}

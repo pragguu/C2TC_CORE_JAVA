572e23639293d72988710a13a4fd2023707bc372
pragguu <aithalprajnar@gmail.com>
2025-09-17 23:56:16 +0530

    First Commit

---
+++ JdbcProjectOne/src/usingstatement/StatementDemo.java
@@ -0,0 +1,13 @@
+package usingstatement;
+
+public class StatementDemo {
+
+	public static void main(String[] args) {
+		//StatementInterfaceDemo obj=new StatementInterfaceDemo();
+		int row=StatementInterfaceDemo.addStudent();
+		System.out.println(row +" is inserted");
+
+
+	}
+
+}
